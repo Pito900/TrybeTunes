779fe74707fafd0b0ee39c5be5a255fd46c4168b
Francisco Monteiro <franciscorubenilto@gmail.com>
2022-02-05 18:14:08 -0300

    Desafio 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,51 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      LookingFor: '',
+      disabSearchButton: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { value } = target;
+    this.setState({
+      LookingFor: value,
+    }, () => this.searchButtonClick());
+  }
+
+  searchButtonClick = () => {
+    const { LookingFor } = this.state;
+    const taNome = 2;
+    if (LookingFor.length >= taNome) {
+      this.setState({ disabSearchButton: false });
+    } else {
+      this.setState({ disabSearchButton: true });
+    }
+  };
+
   render() {
+    const { disabSearchButton } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p> Search </p>
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ this.handleChange }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ disabSearchButton }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
