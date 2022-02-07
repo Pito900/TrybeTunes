a33f83e1aba0b5e8a25617f4ab1b6954d3b1fdb2
Francisco Monteiro <franciscorubenilto@gmail.com>
2022-02-07 13:36:14 -0300

    Desafio 6 - fiquei um tempo para entender que faltava um espaço- fiz comentário nesse ponto

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
+      carregando: false,
       LookingFor: '',
       disabSearchButton: true,
+      listAlbuns: [],
+      respostaAPI: false,
+      nameArtist: '',
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -28,25 +34,86 @@ class Search extends React.Component {
     }
   };
 
+  searchButtonBanda = async () => {
+    const { LookingFor } = this.state;
+    this.setState({
+      carregando: true,
+      nameArtist: LookingFor,
+    });
+    const albuns = await searchAlbumsAPI(LookingFor);
+    this.setState({
+      carregando: false,
+      LookingFor: '',
+      disabSearchButton: true,
+      listAlbuns: albuns,
+      respostaAPI: true,
+    });
+  }
+
   render() {
-    const { disabSearchButton } = this.state;
+    const {
+      disabSearchButton,
+      listAlbuns,
+      respostaAPI,
+      carregando,
+      nameArtist,
+    } = this.state;
+    const cabecalho = (
+      <h2>
+        Resultado de álbuns de:
+        {' '}
+        { nameArtist }
+      </h2>);
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <input
-            type="text"
-            data-testid="search-artist-input"
-            onChange={ this.handleChange }
-          />
-          <button
-            type="submit"
-            data-testid="search-artist-button"
-            disabled={ disabSearchButton }
-          >
-            Pesquisar
-          </button>
-        </form>
+        { carregando ? (
+          <p>Carregando...</p>
+        ) : (
+          <form>
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              onChange={ this.handleChange }
+            />
+            <button
+              type="submit"
+              data-testid="search-artist-button"
+              disabled={ disabSearchButton }
+              onClick={ this.searchButtonBanda }
+            >
+              Pesquisar
+            </button>
+            {respostaAPI && cabecalho}
+            {respostaAPI && listAlbuns.length !== 0 && listAlbuns.map(
+              ({
+                artistName,
+                collectionName,
+                artworkUrl100,
+                trackCount,
+                collectionId,
+              }) => (
+                <section key={ collectionId }>
+                  <h3>{`Resultado de álbuns de: ${artistName}`}</h3>
+                  <img
+                    src={ artworkUrl100 }
+                    alt={ collectionName }
+                  />
+                  <h3>{ collectionName }</h3>
+                  <h3>{ artistName }</h3>
+                  <h3>{ trackCount }</h3>
+                  <Link
+                    to={ `/album/${collectionId}` }
+                    data-testid={ `link-to-album-${collectionId}` }
+                  >
+                    clique para mais
+                  </Link>
+                </section>
+              ),
+            )}
+          </form>
+        )}
+        { respostaAPI && listAlbuns.length === 0 && <p>Nenhum álbum foi encontrado</p> }
       </div>
     );
   }
